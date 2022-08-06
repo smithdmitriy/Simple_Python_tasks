49baa5896f8f6b37eaac7d99a73427066d290c86
smithdmitriy <smithdmitriy@gmail.com>
2022-08-06 11:11:48 +0500

    add 5.2

---
+++ Task 5.2.py
@@ -0,0 +1,14 @@
+questions = [
+"We don't serve strings around here. Are you a string?",
+"What is said on Father's Day in the forest?",
+"What makes the sound 'Sis! Boom! Bah!'?"
+]
+answers = [
+"An exploding sheep.",
+"No, I'm a frayed knot.",
+"'Pop!' goes the weasel."
+]
+
+print(f'\nQ: {questions[0]} \nA: {answers[1]}')
+print(f'\nQ: {questions[1]} \nA: {answers[2]}')
+print(f'\nQ: {questions[2]} \nA: {answers[0]}')
