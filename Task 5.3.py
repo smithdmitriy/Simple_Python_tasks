c709bdb1c2b0ab448f108b1942f63768927840ae
smithdmitriy <smithdmitriy@gmail.com>
2022-08-06 11:47:10 +0500

    add 5.3

---
+++ Task 5.3.py
@@ -0,0 +1,10 @@
+print("My kitty cat likes %s" % 'roast beef')
+print("My kitty cat likes %s" % 'ham')
+print("My kitty cat likes %s" % 'head')
+print("My kitty cat likes %s" % 'clam\n')
+
+
+print("My kitty cat likes %s" % "roast beef\n",
+      "My kitty cat likes %s" % "ham\n",
+      "My kitty cat likes %s" % "head\n",
+      "My kitty cat likes %s" % "clam\n", sep='')
