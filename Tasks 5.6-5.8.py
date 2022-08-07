8a6e96e175404f20276e42641048bd4f251df925
smithdmitriy <smithdmitriy@gmail.com>
2022-08-07 07:22:32 +0500

    add 5.4-5.5

---
+++ Tasks 5.6-5.8.py
@@ -0,0 +1,19 @@
+'''После проведения публичных опросов с целью выбора имени появились: английская подводная лодка Boaty McBoatface,
+ австралийская беговая лошадь Horsey McHorseface и шведский поезд Trainy McTrainface.
+  Используйте форматирование с символом % для того, чтобы вывести на экран победившие имена для утки, тыквы и шпица.'''
+
+print("%sy Mc%sface" % ("Duck", "Duck"))
+print("%sy Mc%sface" % ("Pumpkin", "Pumpkin"))
+print("%sy Mc%sface" % ("Schpic", "Schpic"))
+
+print()
+
+print("{}y Mc{}face".format("Duck", "Duck"))
+print("{}y Mc{}face".format("Pumpkin", "Pumpkin"))
+print("{}y Mc{}face".format("Schpic", "Schpic"))
+
+print()
+
+print(f"{'Duck'}y Mc{'Duck'}face")
+print(f"{'Pumpkin'}y Mc{'Pumpkin'}face")
+print(f"{'Schpic'}y Mc{'Schpic'}face")
